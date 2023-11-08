bc6b0f194a7dff9d3570bc80139e5b9e608fd5f7
6531503118 <122964020+6531503118@users.noreply.github.com>
2023-11-08 07:34:24 +0000

    ultimate jet-ray

--- src/main/java/th/mfu/RestaurantController.java
+++ src/main/java/th/mfu/RestaurantController.java
@@ -35,7 +35,7 @@ public class ConcertController {
 
     @Autowired
     MenuRepository menuRepo;
-}
+
 //getmapping to some where
 @GetMapping("/restaurant")
 public String listRestaurant(Model Model) {
@@ -53,4 +53,21 @@ public String saveRestaurants(@ModelAttribute Restaurant newRestaurant) {
       return "redirect:/restaurant";
 }
 @Transactional
+@GetMapping("/delete-restaurant/{rest_id}")
+public String deleteRestaurant(@PathVariable int rest_id){
+    // งงบรรทัดนี้ แปะไว้ก่อน 
+    restRepo.deleteByRest_id(rest_id);
+    return "redirect:/restaurant";
+}
+//เหมือนจะข้่ามไรไปสักอย่าง แปะไว้ก่อน
+@PostMapping("/restaurant/{rest_id}")
+public String saveRestaurant(@ModelAttribute Restaurant newrestaurant,@PathVariable int rest_id) {     
+    Restaurant restaurant = rest_Repo.findById(rest_id).get();     
+    newrestaurant.setRestaurant(restaurant);    
+    restRepo.save(newrestaurant);
+    return "redirect:/restaurant/"+rest_id+"";
+    // มันดูเเหม่งๆ เเต่ไว้มาเเก้คืนนี้
+}
+}
+
 
