5ec9356b342cd02ad961a20b19fd5ff79bd72563
6531503118 <122964020+6531503118@users.noreply.github.com>
2023-11-26 09:37:46 +0000

    gg

---
+++ src/main/java/th/mfu/Service/UerServiceImpl.java
@@ -0,0 +1,78 @@
+package com.example.LoginAndRegistWeb.Service;
+
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.LoginAndRegistWeb.Entity.User;
+import com.example.LoginAndRegistWeb.Entity.UserRegistrationDto;
+import com.example.LoginAndRegistWeb.Repo.UserRepository;
+import com.example.LoginAndRegistWeb.Role.Role;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    // Injecting UserRepository using @Autowired annotation
+    @Autowired
+    private UserRepository userRepository;
+
+    // Injecting BCryptPasswordEncoder using @Autowired annotation
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+	
+
+
+    // Method for saving user registration information
+    @Override
+    public User save(UserRegistrationDto registrationDto) {
+       
+            // Creating a new User with the provided registration information
+            // and adding the "USER_ROLE" to its roles
+            User user = new User(
+                    registrationDto.getFirstName(),
+                    registrationDto.getLastName(),
+                    registrationDto.getEmail(),
+                    passwordEncoder.encode(registrationDto.getPassword()),
+                    Arrays.asList(new Role("USER_ROLE"))
+            );
+            return userRepository.save(user);
+        
+    }
+
+    // Method for loading user details by username for authentication
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Finding the user by email in the userRepository
+        User user = userRepository.findByEmail(username);
+        // If user not found, throwing an exception
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        // Creating UserDetails object using user information and roles
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles())
+        );
+    }
+
+    // Private method to map roles to authorities
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        // Mapping roles to SimpleGrantedAuthority and collecting them into a list
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+	
+
+
+
+}
